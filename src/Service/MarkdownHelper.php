d0f57e02dd182e9d57c7e631e5c0e456aa68fc5b
nadhemjbeli <nadhem98jbeli@gmail.com>
2022-02-05 17:40:11 +0100

    Config Parameters

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -13,11 +13,14 @@ class MarkdownHelper
     private $cache;
     private $markdown;
     private $logger;
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $logger)
+    private $isDebug;
+
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger, bool $isDebug)
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
-        $this->logger = $logger;
+        $this->logger = $markdownLogger;
+        $this->isDebug = $isDebug;
     }
 
     public function parse(string $source):string
@@ -25,6 +28,11 @@ class MarkdownHelper
         if(stripos($source, 'bacon')!==false){
             $this->logger->info('they are talking about bacon again!');
         }
+
+        if($this->isDebug){
+            return $this->markdown->transform($source);
+        }
+//        dump($this->cache);die;
         $item = $this->cache->getItem('markdown_'.md5($source));
         if (!$item->isHit()) {
             $item->set($this->markdown->transform($source));
