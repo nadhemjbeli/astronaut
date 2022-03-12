c6dc7da351a6d8df3203ffe70c3af844b460273c
nadhemjbeli <nadhem98jbeli@gmail.com>
2022-03-12 01:21:12 +0100

    comments - Querry OR Logic

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,6 @@ return [
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Sonata\Doctrine\Bridge\Symfony\SonataDoctrineSymfonyBundle::class => ['all' => true],
+    Sonata\Twig\Bridge\Symfony\SonataTwigSymfonyBundle::class => ['all' => true],
 ];
