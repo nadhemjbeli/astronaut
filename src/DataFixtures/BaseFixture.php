efe4030a0a8c13a6acf9a73e259d65bbac176adb
nadhemjbeli <nadhem98jbeli@gmail.com>
2022-03-13 21:59:40 +0100

    authenticating - customizing the user entity

--- src/DataFixtures/BaseFixture.php
+++ src/DataFixtures/BaseFixture.php
@@ -66,4 +66,17 @@ abstract class BaseFixture extends Fixture
 
         return $references;
     }
+    protected function createManyUsers(int $count, string $groupName, callable $factory)
+    {
+        for ($i = 0; $i < $count; $i++) {
+            $entity = $factory($i);
+            if (null === $entity) {
+                throw new \LogicException('Did you forget to return the entity object from your callback to BaseFixture::createMany()?');
+            }
+            $this->manager->persist($entity);
+            // store for usage later as groupName_#COUNT#
+            $this->addReference(sprintf('%s_%d', $groupName, $i), $entity);
+        }
+    }
+
 }
