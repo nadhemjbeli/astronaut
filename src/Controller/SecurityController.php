523777a97fe8ade98ef00b1318f0e47a3f8d8b05
nadhemjbeli <nadhem98jbeli@gmail.com>
2022-03-13 23:58:18 +0100

    authenticating - redirecting on success & the user provider

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,12 +12,14 @@ class SecurityController extends AbstractController
     /**
      * @Route("/login", name="app_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils)
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
+
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error'         => $error,
