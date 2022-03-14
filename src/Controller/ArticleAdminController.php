f2c033076fab3a290c0998e33a1f7dd7966f3e30
nadhemjbeli <nadhem98jbeli@gmail.com>
2022-03-14 03:47:19 +0100

    authenticating - IS_AUTHENTICATED

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
