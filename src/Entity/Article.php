a386f1c2ab8d6a299289c136862ed8862ef7e486
nadhemjbeli <nadhem98jbeli@gmail.com>
2022-03-17 00:37:59 +0100

    Forms - Clear that location name data

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -280,6 +280,10 @@ class Article
     {
         $this->location = $location;
 
+        if(!$location || $location === 'interstellar_space'){
+            $this->setSpecificLocationName(null);
+        }
+
         return $this;
     }
 
